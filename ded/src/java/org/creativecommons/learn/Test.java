a8ef2862cc39b50f2e10c9cff4a7ba21c24780a6
Asheesh Laroia <asheesh@asheesh.org>
2010-04-28 14:13:01 -0700

    Trying to write a JUnit test that gets tested via ant.

---
+++ ded/src/java/org/creativecommons/learn/Test.java
@@ -0,0 +1,13 @@
+package org.creativecommons.learn;
+
+import junit.framework.*;
+public class Test extends TestCase {
+    public void testOne()
+    {
+        assertTrue( "TestExample", true );
+    }
+
+    public static void main(String[] args) {
+    	System.out.println("fine");
+    }
+}
