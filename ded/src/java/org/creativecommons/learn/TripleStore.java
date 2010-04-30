ab42ecdf87be46f0997e4fb41ef6d3f42062750d
Nathan R. Yergler <nathan@yergler.net>
2010-04-30 12:01:54 -0700

    Configurable database settings.

--- ded/src/java/org/creativecommons/learn/TripleStore.java
+++ ded/src/java/org/creativecommons/learn/TripleStore.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Properties;
 
 import thewebsemantic.Bean2RDF;
 import thewebsemantic.Filler;
@@ -18,86 +19,88 @@ import com.hp.hpl.jena.rdf.model.ModelMaker;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 /**
- *
+ * 
  * @author nathan
  */
 public class TripleStore {
 
 	private static TripleStore instance = null;
-	
-    private IDBConnection conn = null;
-    private ModelMaker maker = null;
-    private Model model = null;
-    
-    private RDF2Bean loader = null;
-    private Bean2RDF saver = null;
-    
-    private TripleStore() {
-    	// private constructor
-    	super();
-    	
-    	try {
-        	this.loader = new RDF2Bean(this.getModel());
+
+	private IDBConnection conn = null;
+	private ModelMaker maker = null;
+	private Model model = null;
+
+	private RDF2Bean loader = null;
+	private Bean2RDF saver = null;
+
+	private TripleStore() {
+		// private constructor
+		super();
+
+		try {
+			this.loader = new RDF2Bean(this.getModel());
 			this.saver = new Bean2RDF(this.getModel());
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-    
-    public static TripleStore get() {
-    	
-    	if (instance == null) {
-    		instance = new TripleStore();
-    	}
-    	
-    	return instance;
-    }
-    
-    private void open() throws ClassNotFoundException {
-    	
-        String className = "com.mysql.jdbc.Driver";         // path of driver class
-        Class.forName (className);                          // Load the Driver
-        String DB_URL =     "jdbc:mysql://localhost/oercloud?autoReconnect=true";  // URL of database 
-        String DB_USER =   "discovered";                          // database user id
-        String DB_PASSWD = "";                          // database password
-        String DB =        "MySQL";                         // database type
+	}
 
-        // Create database connection
-        conn = new DBConnection ( DB_URL, DB_USER, DB_PASSWD, DB );
-        maker = ModelFactory.createModelRDBMaker(conn) ;
-    	
-    } // open
- 
-    private void close() {
-        try {
-            // Close the database connection
-            conn.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(TripleStore.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    } // close
+	public static TripleStore get() {
 
-    public Model getModel() throws ClassNotFoundException {
+		if (instance == null) {
+			instance = new TripleStore();
+		}
 
-    	if (maker == null) {
-    		this.open();
-    	}
- 
-    	if (model == null) {
-            // create or open the default model
-    		this.model = maker.createDefaultModel();
-    	}
-    	
-        return this.model;
+		return instance;
+	}
 
-    } // getModel
-    
+	private void open() throws ClassNotFoundException {
 
-    /* Delegate Methods */
-    /* **************** */
-    
+		Properties config = DEdConfiguration.load();
+
+		// register the JDBC driver
+		Class.forName(config.getProperty("rdfstore.db.driver")); // Load the Driver
+
+		// Create the Jena database connection
+		this.conn = new DBConnection(
+				config.getProperty("rdfstore.db.url"), 
+				config.getProperty("rdfstore.db.user"), 
+				config.getProperty("rdfstore.db.password"),
+				config.getProperty("rdfstore.db.type"));
+		this.maker = ModelFactory.createModelRDBMaker(conn);
+
+	} // open
+
+	private void close() {
+		try {
+			// Close the database connection
+			conn.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(TripleStore.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+
+	} // close
+
+	public Model getModel() throws ClassNotFoundException {
+
+		if (maker == null) {
+			this.open();
+		}
+
+		if (model == null) {
+			// create or open the default model
+			this.model = maker.createDefaultModel();
+		}
+
+		return this.model;
+
+	} // getModel
+
+	/* Delegate Methods */
+	/* **************** */
+
 	public boolean exists(Class<?> c, String id) {
 		return loader.exists(c, id);
 	}
