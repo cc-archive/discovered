1b78cf831b68f9b2ba61143d6e1e4c0e96c3b1a2
Asheesh Laroia <asheesh@asheesh.org>
2010-06-02 15:31:49 -0400

    Adding tests that have been moved out into a separate package.

---
+++ ded/src/java/org/creativecommons/learn/test/TestCuratorFeed.java
@@ -0,0 +1,150 @@
+package org.creativecommons.learn.test;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import org.creativecommons.learn.RdfStore;
+import org.creativecommons.learn.oercloud.Curator;
+import org.creativecommons.learn.oercloud.Feed;
+import org.creativecommons.learn.oercloud.Resource;
+
+public class TestCuratorFeed extends DiscoverEdTestCase {
+	
+	/** A unit test that shows adding a curator works. 
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException */
+	public void addCurator() throws SQLException, ClassNotFoundException {
+		String graphName = "http://creativecommons.org/#site-configuration";
+		RdfStore store = RdfStore.uri2RdfStore(graphName);
+		
+		/* We have no Curators at the start */
+		Collection<Curator> available_curators = store.load(org.creativecommons.learn.oercloud.Curator.class);
+		assertEquals(0, available_curators.size());
+
+		/* Create a Curator, as if we were using the command line */
+		org.creativecommons.learn.feed.AddCurator.addCurator("Notre Dame OCW", "http://ocw.nd.edu/");
+
+		available_curators = store.load(org.creativecommons.learn.oercloud.Curator.class);
+		assertEquals(1, available_curators.size());
+		
+		/* Make sure we saved it correctly */
+		Curator curator = available_curators.iterator().next();
+		assertEquals(curator.getName(), "Notre Dame OCW");
+		assertEquals(curator.getUrl(), "http://ocw.nd.edu/");
+		
+		/* Get a different RdfStore */
+		RdfStore aDifferentStore = RdfStore.uri2RdfStore("http://other.example.com/#site-configuration");
+		
+		/* We have no Curators in the different RdfStore */
+		Collection<Curator> aDifferentListOfCurators = aDifferentStore.load(org.creativecommons.learn.oercloud.Curator.class);
+		assertEquals(0, aDifferentListOfCurators.size());
+		store.close();
+	}
+	
+	public void testAddCurator() throws SQLException, ClassNotFoundException {
+		this.addCurator();
+	}
+	
+	/** A unit test that shows adding a curator works. 
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException */
+	public void testAddFeed() throws SQLException, ClassNotFoundException {
+		
+		RdfStore store = RdfStore.uri2RdfStore("http://creativecommons.org/#site-configuration");
+		this.addCurator();
+
+		/* We have no Feeds at the start */
+		Collection<Feed> available_feeds = store.load(org.creativecommons.learn.oercloud.Feed.class);
+		assertEquals(0, available_feeds.size());
+
+		/* Create a Feed, as if we were using the command line */
+		org.creativecommons.learn.feed.AddFeed.addFeed("rss", "http://ocw.nd.edu/courselist/rss", "http://ocw.nd.edu/");
+
+		available_feeds = store.load(org.creativecommons.learn.oercloud.Feed.class);
+		assertEquals(1, available_feeds.size());
+		
+		/* Make sure we saved it correctly */
+		Feed feed = available_feeds.iterator().next();
+		assertEquals(feed.getCurator().getUrl(), "http://ocw.nd.edu/");
+		assertEquals(feed.getFeedType(), "rss");
+		assertEquals(feed.getUrl(), "http://ocw.nd.edu/courselist/rss");
+	}
+	
+	/** 
+	You have an RSS feed in the site-configuration triple store with
+	such-and-such a curator,
+	The feed includes a particular resource
+	The feed gives that resource a dc:title
+	You aggregate
+	You query the **feed**'s store for the resource
+	You can access the dc:title with getTitle
+	*/	
+	public void testURLTitleProvenance() throws SQLException {
+		// Add a curator		
+		String curatorURI = "http://ocw.nd.edu/";
+		org.creativecommons.learn.feed.AddCurator.addCurator("Notre Dame OpenCourseWare", curatorURI);
+		
+		// Add a feed to that curator's triple store. This feed says, in effect, "here's a URL, and here's its title".
+		String feedURI = "http://ocw.nd.edu/courselist/rss";
+		org.creativecommons.learn.feed.AddFeed.addFeed("rss", feedURI, curatorURI);
+		
+		// Aggregate
+		String[] args = {};
+		org.creativecommons.learn.aggregate.Main.main(args);
+		
+		// Query that curator's RdfStore, find the triple URI-title-literal
+		RdfStore store = RdfStore.uri2RdfStore(feedURI);
+		Collection<Resource> resources = store.load(org.creativecommons.learn.oercloud.Resource.class);
+		Resource r = resources.iterator().next();
+		String title = r.getTitle();
+		assertTrue(title.length() > 0);
+	}
+
+	// X: Curator and OAI-PMH Feed
+	// Y: Aggregate
+	// Z: The title of a Resource found by aggregating data from the feed
+	// A: The URI of the Curator of the Feed
+
+	public void testIntegration()
+    {
+    	// Steps:
+    	// 1. Add a curator to the triple store
+    	// 2. Add a feed to the triple store
+    	// 3. (templated on type of feed) aggregate package pulls data in
+    	// ...? 
+        assertTrue( "TestExample", true );
+    }
+
+    public void testAddFeedMustPointToCuratorWithinTheStore() throws SQLException {
+    	try {
+			RdfStore store = RdfStore.getSiteConfigurationStore();
+			
+			/* Try adding a feed where we have no data stored about
+			 * the curator. */
+			Collection<Feed> available_feeds = store.load(org.creativecommons.learn.oercloud.Feed.class);
+			assertEquals(0, available_feeds.size());
+	
+			/* Create a Feed, as if we were using the command line */
+			org.creativecommons.learn.feed.AddFeed.addFeed("rss", "http://ocw.nd.edu/courselist/rss", "http://ocw.nd.edu/");
+    	} catch (IllegalArgumentException e) {
+    		// sweet.
+    		return;
+    	}
+    	assertFalse(true);
+	}
+    
+	/**
+	 * You have a OPML feed in the site-configuration triple store with
+	 * such-and-such a curator
+	 * The feed includes a particular resource
+	 * The OPML feed can't make a dc:title assertion about the resource
+	 * But the OPML file contains an RSS feed that makes a dc:title assertion about a Resource
+	 * You aggregate
+	 * You query that **RSS feed**'s store for the resource FIXME: Maybe it should be the OPML feed?
+	 * You can access the dc:title with getTitle
+	 */    
+    public void testTitleKnowsItsOPMLFeedProvenance() {
+    
+    }
+
+}
---
+++ ded/src/java/org/creativecommons/learn/test/TestRDFaTitle.java
@@ -0,0 +1,90 @@
+package org.creativecommons.learn.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+
+import junit.framework.TestCase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.creativecommons.learn.RdfStore;
+import org.creativecommons.learn.oercloud.Resource;
+
+public class TestRDFaTitle extends DiscoverEdTestCase {
+
+	public void testResourcesCanGiveThemselvesATitle() throws SQLException {
+		// Let's say we're aggregating a Resource with URI "http://example.com/barbie" and dc:title "My Barbie Land"
+		
+		// So that means having this triple:
+		// <http://example.com/barbie> <dc:title> "My Barbie Land"
+		
+		// With what provenance are these triples stored? (Note that all triples must have a provenance
+		// because otherwise we wouldn't know what Jena triple store to look at.)
+		
+		// Let's store the triple with the provenance of "http://example.com/barbie"
+		
+		// Add a curator
+		String curatorURI = "http://example.com/";
+		org.creativecommons.learn.feed.AddCurator.addCurator("This doesn't matter for this test", curatorURI);
+		
+		// Add a feed to that curator's triple store. This feed says, in effect, "here's a URL".
+		String testURIPrefix = "http://a6.creativecommons.org/~raffi/html_for_discovered_unit_tests/";
+		String feedURI = testURIPrefix + "rss_pointing_to_i_know_my_title.xml";
+		org.creativecommons.learn.feed.AddFeed.addFeed("rss", feedURI, curatorURI);
+		
+		// Aggregate
+		String[] args = {};
+		org.creativecommons.learn.aggregate.Main.main(args);
+		
+		// Query the RdfStore corresponding to the feed. Find the triple URI, title, literal.
+		RdfStore feedStore = RdfStore.uri2RdfStore(feedURI);
+		Collection<Resource> resourcesInFeedStore = feedStore.load(org.creativecommons.learn.oercloud.Resource.class);
+		assertEquals(1, resourcesInFeedStore.size());
+		Resource resourceInFeedStore = resourcesInFeedStore.iterator().next();
+		
+		// Just double check that the feed store's copy of the resource doesn't have a title
+		assertEquals(null, resourceInFeedStore.getTitle());
+		
+		// Now query the RdfStore for r.getURL()
+		// This will give us the same resource, but when we access its getTitle method we'll see the title it uses to describe itself
+		String resourceURI = resourceInFeedStore.getUrl();
+		assertEquals(testURIPrefix + "i_know_my_title.html", resourceURI);
+		RdfStore store = RdfStore.uri2RdfStore(resourceURI);
+		Collection<Resource> resources = store.load(org.creativecommons.learn.oercloud.Resource.class);
+		assertEquals(1, resources.size());
+		Resource resourceInItsOwnStore = resources.iterator().next();
+		
+		// Now there should be a title
+		assertEquals("This title is provided by the page itself", resourceInItsOwnStore.getTitle());
+		
+	}
+	
+	/*
+	 * 
+	 * We should write these tests:
+
+
+	You have a OAI-PMH feed in the site-configuration triple store with
+	such-and-such a curator,
+	and the feed includes a particular resource,
+	and the feed gives that resource a dc:title
+	You aggregate,
+	You query that **feed**'s store for the resource
+	You can access the dc:title with getTitle
+	
+		The OAI-PMH client library might have some tests.
+
+	You have a RSS feed in the site-configuration triple store with
+	such-and-such a curator
+	The feed contains a resource whose HTML content specifies a dc:title in RDFa
+	You aggregate,
+	You query that **resource**'s store for the resource
+	You can access the dc:title with getTitle
+
+	We're likely to test just the dc:title, since such a test passes, the
+	other metadata are surely stored with the same/correct provenance as
+	well.
+	*/
+}
