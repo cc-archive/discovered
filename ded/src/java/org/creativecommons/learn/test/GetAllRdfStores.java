14e7e495e8cad2821a7e5e7946e8eb26c6b02949
Raphael Krut-Landau <raphael@openhatch.org>
2010-06-08 17:34:55 -0400

    Write a test that we correctly-handle grabbing the same RdfStore. (It fails.)

--- ded/src/java/org/creativecommons/learn/test/GetAllRdfStores.java
+++ ded/src/java/org/creativecommons/learn/test/GetAllRdfStores.java
@@ -13,4 +13,12 @@ public class GetAllRdfStores extends DiscoverEdTestCase {
 		assertEquals(RdfStore.SITE_CONFIG_URI, uris.get(0));
 	}
 
+	public static void testWorksTheSecondTime() throws SQLException {
+		RdfStore store = RdfStore.getSiteConfigurationStore();
+		RdfStore the_same_store = RdfStore.getSiteConfigurationStore();
+		List<String> uris = RdfStore.getAllKnownTripleStoreUris();
+		assertEquals(RdfStore.SITE_CONFIG_URI, uris.get(0));
+		assertEquals(uris.size(), 1);
+	}
+
 }
