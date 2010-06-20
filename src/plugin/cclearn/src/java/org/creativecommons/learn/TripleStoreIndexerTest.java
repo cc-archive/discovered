ee285d4957515e0f496aa362f3162b044d403015
Asheesh Laroia <asheesh@asheesh.org>
2010-06-20 18:29:22 -0400

    Begin to write a test that we can generate a list of available Lucene columns.

---
+++ src/plugin/cclearn/src/java/org/creativecommons/learn/TripleStoreIndexerTest.java
@@ -0,0 +1,29 @@
+package org.creativecommons.learn;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.creativecommons.learn.oercloud.Resource;
+
+import junit.framework.TestCase;
+
+public class TripleStoreIndexerTest extends TestCase {
+	public static void testGenerateAllPossibleColumnNames() {
+		// first, create a Triple (admittedly, in the siteConfigurationStore) that
+		// has a predicate that's attached to a Resource.
+		Resource r = new Resource("http://example.com/#resource");
+		r.setTitle("A title");
+		TripleStore store = TripleStore.get();
+		store.save(r);
+		
+		// Now, ask the TripleStoreIndexer what column names it has.
+		
+		// prepare list of known...
+		Collection<String> expected = new ArrayList<String>();
+		
+		TripleStoreIndexer indexer = new TripleStoreIndexer();
+		
+		Collection<String> got = indexer.getAllPossibleColumnNames();
+		assertEquals(expected, got);
+	}
+}
