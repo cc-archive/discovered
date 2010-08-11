526d766c357498e1a495fcc7f14b8be5dd79cb1d
Nathan R. Yergler <nathan@yergler.net>
2010-08-11 16:27:39 -0700

    Fleshed out javadoc for MetadataRetriever interface.

--- ded/src/java/org/creativecommons/learn/plugin/MetadataRetriever.java
+++ ded/src/java/org/creativecommons/learn/plugin/MetadataRetriever.java
@@ -2,15 +2,25 @@ package org.creativecommons.learn.plugin;
 
 import org.creativecommons.learn.oercloud.Resource;
 
+/**
+ * 
+ * The plugin interface for modifying {@link Resource}s as they are processed 
+ * during aggregation. 
+ * 
+ * @author Nathan R. Yergler <nathan@yergler.net>
+ * @see Resource
+ * @see org.creativecommons.learn.aggregate.FeedUpdater
+ * 
+ */
 public interface MetadataRetriever {
 	  /** The name of the extension point. */
 	  final static String X_POINT_ID = MetadataRetriever.class.getName();
 
 	  /**
-	   * Retrieve metadata for the Resource.  Return a new or modified Resource.
+	   * Retrieve metadata for the {@link Resource}.  Return a new or modified Resource.
 	   * 
 	   * @param resource aggregated Resource.
-	   * @return modified (or a new) Reource containing added metadata
+	   * @return modified (or a new) Resource containing added metadata
 	   */
 	  Resource retrieve(Resource resource);
 
