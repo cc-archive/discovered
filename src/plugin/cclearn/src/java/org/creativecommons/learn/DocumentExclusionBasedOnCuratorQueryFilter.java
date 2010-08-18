4c9fb67841f038921d6e0e3046c0801569aa823d
Asheesh Laroia <asheesh@asheesh.org>
2010-08-18 16:00:55 -0400

    Simplify extraction of curator URIs to exclude
    Now that the field is raw, we do not have to do the insane trick where
    we guessed what the user inputted by attempting to the tokenization performed
    by Nutch.

--- src/plugin/cclearn/src/java/org/creativecommons/learn/DocumentExclusionBasedOnCuratorQueryFilter.java
+++ src/plugin/cclearn/src/java/org/creativecommons/learn/DocumentExclusionBasedOnCuratorQueryFilter.java
@@ -13,7 +13,6 @@ import org.apache.nutch.searcher.Query;
 import org.apache.nutch.searcher.QueryException;
 import org.apache.nutch.searcher.QueryFilter;
 import org.apache.nutch.searcher.Query.Clause;
-import org.apache.nutch.searcher.Query.Phrase;
 import org.creativecommons.learn.oercloud.Curator;
 
 public class DocumentExclusionBasedOnCuratorQueryFilter implements QueryFilter {
@@ -27,63 +26,17 @@ public class DocumentExclusionBasedOnCuratorQueryFilter implements QueryFilter {
     
     private HashSet<String> getCuratorsToExclude(Clause[] clauses) {
     	HashSet<String> ret = new HashSet<String>();
-		for (Clause c: clauses) {
-			// FIXME: Use "-curator", not "excludecurator". We're just avoiding
-			// the minus sign to make things easier at this stage of
-			// development.
-			if (c.getField().equals("excludecurator")) {
-				/* NOTE: This is some batty code.
-				 * 
-				 * As far as I can tell, Nutch will preprocess the following query:
-				 * 	excludecurator:http://ocw.nd.edu/
-				 * 
-				 *  into ("excludecurator", {"http", "ocw", "nd", "edu"})
-				 *  
-				 * I'm going to de-preprocess it back. I realize that is utter insanity.
-				 * 
-				 * There is this class called RawFieldQueryFilter but I don't yet see how it works.
-				 * 
-				 * I've been advised to read DateQueryFilter but don't yet see how that works differently.
-				 * 
-				 */
-				// Find the value of this field
-				if (c.isPhrase()) {
-					StringBuffer buffer = new StringBuffer();;
-					LOG.info("Got a phrase");
-					Phrase nutchPhrase = c.getPhrase();
-					LOG.info("Phrase has toString: " + nutchPhrase.toString());
-					LOG.info("Clause has toStriong: " + c.toString());
-					Query.Term[] terms = nutchPhrase.getTerms();
-					for (int j = 0; j < terms.length; j++) {
-						// if this is the first term, make sure it is http
-						if (j == 0) {
-							String s = terms[j].toString();
-							if (s.equals("http")) {
-								buffer.append("http://");
-								LOG.info("Added http");
-							}
-							else {
-								LOG.info("Weird, terms[0].toString() wasn't http.");
-							}
-							continue; // next term, then!
-						}
-						
-						// Normally, we suffix term toString values with ".".
-						String suffix = ".";
-						// But the last one should be suffixed with "/"
-						if (j == (terms.length - 1)) {
-							suffix = "/";
-						}
 
-						buffer.append(terms[j].toString());
-						buffer.append(suffix);
-					}
-					ret.add(buffer.toString());
-				} else {
-					LOG.info("Got a term, I guess.");
-					ret.add(c.getTerm().toString());
-				}
-			}
+        for (Clause c: clauses) {
+          // skip any irrelevant clauses
+          if (!c.getField().equals("excludecurator"))
+            continue;
+
+          // Because it is configured as a raw field, all clauses are Terms,
+          // not Clauses. So getTerm() is enough.
+          String curatorURI = c.getTerm().toString();
+          LOG.warn("Picked up on a curator exclusion URI: " + curatorURI);
+          ret.add(curatorURI);
 		}
 		return ret;
     }
